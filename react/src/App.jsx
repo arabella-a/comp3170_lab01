0fdc24565d019c9338755658c73e271686992b6d
Ara Bella <arabella@Aras-MacBook-Pro.local>
2024-09-13 21:31:39 -0700

    add main content

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -8,8 +8,76 @@ function App() {
           </nav>
         </header>
         <main>
-          <p>jebewon slay</p>
-        </main>
+          <img src="./assests/zb1.jpeg" alt="zerobaseone group photo"/>
+          <h1>MyTunes</h1>
+          <h3>ZEROBASEONE Discography</h3>
+          <div>
+            <table className="table">
+              <tr>
+                <th>YOUTH IN THE SHADE</th>
+              </tr>
+              <tr>
+                <td>Back to ZEROBASE</td>
+                <td>In Bloom</td>
+                <td>New Kidz on the Block</td>
+                <td>And I</td>
+                <td>Our Season</td>
+                <td>Always</td>
+              </tr>
+            </table>
+            <table className="table">
+              <tr>
+                <th>MELTING POINT</th>
+              </tr>
+              <tr>
+                <td>MELTING POINT</td>
+                <td>Take My Hand</td>
+                <td>CRUSH</td>
+                <td>And I</td>
+                <td>Our Season</td>
+                <td>Always</td>
+              </tr>
+            </table>
+            <table className="table">
+              <tr>
+              <th>ゆらゆら -運命の花 YURA YURA</th>
+              </tr>
+              <tr>
+                <td>ゆらゆら -運命の花 YURA YURA</td>
+                <td>In Bloom - Japanese Ver.</td>
+                <td>Crush - Japanese Ver.</td>
+              </tr>
+            </table>
+            <table className="table">
+              <tr>
+                <th>You had me at HELLO</th>
+              </tr>
+              <tr>
+                <td>Solar POWER</td>
+                <td>Feel the POP</td>
+                <td>Dear ECLIPSE</td>
+                <td>SWEAT</td>
+                <td>Sunday RIDE</td>
+                <td>HELLO</td>
+                <td>Feel the POP - Sped Up Version</td>
+              </tr>
+            </table>
+            <table className="table">
+              <tr>
+                <th>CINEMA PARADISE</th>
+              </tr>
+              <tr>
+                 <td>GOOD SO BAD</td>
+                <td>KILL THE ROMEO</td>
+                <td>The Sea - ZB1 Remake</td>
+                <td>Insomia</td>
+                <td>Road Movie</td>
+                <td>Eternity</td>
+                <td>YURA YURA - Korean Version</td>
+              </tr>
+            </table>
+          </div>
+         </main>
       </section>
       <footer>
         
